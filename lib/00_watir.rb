8cc58981dcb45a507a6642af58507764d9735044
david115896 <dtaccori@gmail.com>
2019-10-11 18:19:47 +0200

    new commit

---
+++ lib/00_watir.rb
@@ -0,0 +1,8 @@
+#My ruby file watir
+require 'dotenv'
+Dotenv.load('.env')
+require 'pry'
+require 'dotenv'
+def perform
+end
+perform
---
+++ spec/00_watir_spec.rb
@@ -0,0 +1,6 @@
+require_relative '../lib/00watir'
+describe 'my method does' do
+	it 'should do that' do
+		expect(method_name('parameter')).to eq('result_expected')
+	end
+end
